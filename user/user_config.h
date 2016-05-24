82079e43c58a3c75145cd7c2608eabc07163b529
KateiRen <Karsten.Hartlieb@gmx.de>
2016-05-24 19:28:54 +0200

    1st Project Upload

---
+++ user/user_config.h
@@ -0,0 +1,13 @@
+/*
+ * user_config.h
+ *
+ *  Created on: 24.05.2016
+ *      Author: Karsten
+ */
+
+#ifndef USER_USER_CONFIG_H_
+#define USER_USER_CONFIG_H_
+
+
+
+#endif /* USER_USER_CONFIG_H_ */
---
+++ user/user_main.c
@@ -0,0 +1,14 @@
+/*
+ * user_main.c
+ *
+ *  Created on: 24.05.2016
+ *      Author: Karsten
+ */
+
+#include <osapi.h>
+
+
+void user_init(void)
+{
+	os_delay_us(1000000);
+}
